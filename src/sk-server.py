44623f8b468c95b9c19257a4bf2105854df4fb36
Daniel <danie.aragon.0235@hotmail.com>
2019-10-03 22:58:38 -0500

    frta

--- src/sk-server.py
+++ src/sk-server.py
@@ -21,23 +21,6 @@ class Detector:
         self.tfnet = None
         self.capture = None
 
-    async def proccess(self, ret, frame):
-        if ret:
-            results = self.tfnet.return_predict(frame)
-            for color, result in zip(colors, results):
-                tl = (result['topleft']['x'], result['topleft']['y'])
-                br = (result['bottomright']['x'], result['bottomright']['y'])
-                label = result['label']
-                confidence = result['confidence']
-                text = '{}: {:.0f}%'.format(label, confidence * 100)
-                frame = cv2.rectangle(frame, tl, br, color, 5)
-                frame = cv2.putText(
-                    frame, text, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2) 
-            cv2.imshow('frame',frame)
-            print(data)
-            #socketio.emit('results', json.dumps(str(results)))
-            # print('FPS {:.1f}'.format(1 / (time.time() - stime)))
-
     def detect(self):
         # stime = time.time()
         if self.capture == None and self.tfnet == None:
@@ -47,8 +30,38 @@ class Detector:
         ret, frame = self.capture.read()
         ret, buffer = cv2.imencode('.jpg', frame)
         img = base64.b64encode(buffer)
-        socketio.emit('wcap', img.decode("utf-8"))
+        #socketio.emit('wcap', img.decode("utf-8"))
         asyncio.run(self.proccess(ret, frame))
+
+    async def proccess(self, ret, frame):
+        if ret:
+            data = []
+            results = self.tfnet.return_predict(frame)
+            for color, result in zip(colors, results):
+                tl = (result['topleft']['x'], result['topleft']['y'])
+                br = (result['bottomright']['x'], result['bottomright']['y'])
+                label = result['label']
+                confidence = result['confidence']
+                text = '{}: {:.0f}%'.format(label, confidence * 100)
+                frame = cv2.rectangle(frame, tl, br, color, 5)
+                frame = cv2.putText(
+                    frame, text, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+                data.append({
+                    "label": result['label'],
+                    "confidence": f'{result["confidence"]}',
+                    "topleft": {
+                        "x": result['topleft']['x'],
+                        "y": result['topleft']['y']
+                    },
+                    "bottomright": {
+                        "x": result['bottomright']['x'],
+                        "y": result['bottomright']['y']
+                    }
+                })
+            print(results)
+            if results != []:
+                socketio.emit('results', json.dumps(data))
+            # print('FPS {:.1f}'.format(1 / (time.time() - stime)))
 
 
 detector = Detector()
@@ -56,7 +69,7 @@ options = {
     'model': 'assets/yolov2-tiny.cfg',
     'load': 'assets/yolov2-tiny_8000.weights',
     'labels': 'assets/classes.txt',
-    'threshold': 0.2,
+    'threshold': 0.1,
     'gpu': 1.0
 }
 colors = [tuple(255 * np.random.rand(3)) for _ in range(10)]
@@ -70,18 +83,19 @@ def sessions():
 def listen():
     while True:
         detector.detect()
-        eventlet.sleep(0.1)
+        eventlet.sleep(1)
 
 
 @socketio.on('connect')
 def on_connection():
     print("connected")
-    global detector
     detector.tfnet = TFNet(options)
     detector.capture = cv2.VideoCapture(0)
     detector.capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     detector.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-    eventlet.spawn(listen)
+
+
+eventlet.spawn(listen)
 
 if __name__ == '__main__':
     socketio.run(app, "127.0.0.1", 5000, debug=True)
